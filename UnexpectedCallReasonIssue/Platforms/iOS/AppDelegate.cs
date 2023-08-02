f6cacf7d0811c82c6c210aae2573e8ce02ab6a2a
WladimirKröker <wladimir.kroeker@mateco.eu>
2023-08-02 10:00:12 +0200

    Add project

---
+++ UnexpectedCallReasonIssue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace UnexpectedCallReasonIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
